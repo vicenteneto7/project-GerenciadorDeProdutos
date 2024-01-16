4b16600e4948e9556e205e32fef5a6e6e4a596d5
Vicente <vicentefmm2019@gmail.com>
2024-01-16 09:02:11 -0300

    fix: remoção de arquivos

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -13,10 +13,10 @@
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('tema/plugins/jquery/jquery.min.js') ?>"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('tema/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?= base_url('tema/dist/js/adminlte.min.js') ?>"></script>
 </body>
 </html>
